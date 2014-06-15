43fa7fb9a0a1e5d0592dfecf94126cac04caa91b
rkuttan <rkuttan@users.noreply.github.com>
2014-06-15 22:41:50 +0530

    Enhanced and added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,63 @@
-readVariables <- function() {
-  
-  featureList <- read.table("features.txt",sep=" ", 
-                            col.names=c("id","Variable"))
-  regEx <- "[Mm]ean\\(\\)|std\\(\\)"
-  matchIndices <- grep(pattern=regEx,featureList$Variable)
-  filterList <- featureList[matchIndices,]
-  filterList
-  
+# Read the measurement names from the feattures.txt file
+readVariables <- function() {  
+  featureList<-read.table("features.txt",sep=" ", 
+             col.names=c("id","Variable"))  
+  featureList
 }
 
-combineObservation <- function(observationtype, variables) {  
+#Use a regular expression to filter variable names by mean() and std()
+filterVariables <- function(featureList){
+  regEx <- "[Mm]ean\\(\\)|std\\(\\)"
+  matchIndices <- grep(pattern=regEx,featureList$Variable)
+  #filterList <- featureList[matchIndices,]
+  #filterList
+  featureList[matchIndices,]
+}
+
+#Combine the observation for an observationtype i.e. test or train
+#Read the test/training data into a data frame, the subjects and the activities.
+#Combine all these into a single data frame.
+combineObservation <- function(observationtype, measurement) {  
+  
+  #Read all the subjects for each observation
   subject <- read.table(paste(observationtype,"/subject_",observationtype,".txt",sep=""),sep=" ",col.names=c("Subject"))    
-  observations <- read.table(paste(observationtype,"/X_",observationtype,".txt",sep=""),sep=" ",fill=TRUE,row.names=NULL,header=FALSE, nrows=nrow(subject))                               
-  filteredObservation <- observations[,variables$id]
+  
+  #Read the actual measurements and filter out only the mean() and std() measurements.
+  observations <- read.table(paste(observationtype,"/X_",observationtype,".txt",sep=""),header=FALSE, col.names=measurement$Variable)                               
+  filteredObservation <- filterVariables(measurement)
+  observations <- observations[,filteredObservation$Variable]
+  
+  #Read all the activities for each observation
   activity <- read.table(paste(observationtype,"/y_",observationtype,".txt",sep=""),sep="",col.names=c("Activity"))  
-  combinedobs<-cbind(filteredObservation,subject,as.factor(activity$Activity))
-  colnames(combinedobs) <- c(as.character(variables$Variable),"Subject","Activity")
+  
+  #Combine observations, subject and activity. Store activities as factors.
+  combinedobs<-cbind(observations,subject,as.factor(activity$Activity))
+  colnames(combinedobs) <- c(as.character(filteredObservation$Variable),"Subject","Activity")
+  
+  #Rename the levels of the activities to descriptive names as indicated in activity_labels.txt
   levels(combinedobs$Activity)<-c("Walking","Walking Upstairs","Walking downstairs","Sitting","Standing","Laying")
   combinedobs
 }
 
+#Combines observations from the test and training into a single data frame
+# and aggregates it by activity and subject
 combineBothObservations <- function() {
-  filterList<-readVariables()
-  testObservations <- combineObservation("test",filterList)
-  trainingObservations <- combineObservation("train",filterList)
-  combinedallobs<- rbind(testObservations,trainingObservations)
+  #Read all the measurements
+  featureList<-readVariables()
+  
+  #Read the test and training observations
+  testObservations <- combineObservation("test",featureList)
+  trainingObservations <- combineObservation("train",featureList)
+  
+  #Combine both results
+  combinedallobs<- rbind(testObservations,trainingObservations)  
+  combinedallobs
+  
+  #Calculate the mean grouping by activity and subject
   aggregatedObs <- aggregate(combinedallobs[,1:(ncol(combinedallobs)-2)],by=list(combinedallobs$Activity,combinedallobs$Subject), FUN=mean,na.rm=TRUE)
-  colnames(aggregatedObs)<-c("Activity","Subject",as.character(filterList$Variable))
-  aggregatedObs
-  write.table(aggregatedObs,"tidydata.txt")
+  filterList<-filterVariables(featureList)
+  colnames(aggregatedObs)<-c("Activity","Subject",as.character(filterList$Variable))  
+  
+  #Write the tidy data set to a csv file.
+  write.table(aggregatedObs,"tidydata.csv",sep=",",row.names=FALSE)
 }
